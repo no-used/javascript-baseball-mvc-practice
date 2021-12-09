3ef9e2261fac66f7dc89e111c646dfd6761eb356
sangminsong <ssmin0606@hanmail.net>
2021-12-09 15:57:27 +0900

    feat: 사용자 입력값이 1 ~ 9인지 검사하는 기능 구현

--- javascript-baseball-precourse/src/model/validator.js
+++ javascript-baseball-precourse/src/model/validator.js
@@ -1,3 +1,9 @@
+import NUMBER from '../constants/number.js';
+
+const isNumberInRange = (value, minDigit, maxDigit) => {
+  return value >= minDigit && value <= maxDigit;
+};
+
 export const isValiUserInput = (value) => {
-  return !Number.isNaN(Number(value));
+  return !Number.isNaN(Number(value)) && isNumberInRange(value, NUMBER.MIN_DIGIT, NUMBER.MAX_DIGIT);
 };
