775b3b5e92c90758aa102bec4518e37e41670a0a
sangminsong <ssmin0606@hanmail.net>
2021-12-09 16:01:15 +0900

    feat: 사용자 입력값에 중복이 있는 지 검사하는 기능 구현

--- javascript-baseball-precourse/src/model/validator.js
+++ javascript-baseball-precourse/src/model/validator.js
@@ -1,9 +1,20 @@
 import NUMBER from '../constants/number.js';
 
 const isNumberInRange = (value, minDigit, maxDigit) => {
-  return value >= minDigit && value <= maxDigit;
+  // return value >= minDigit && value <= maxDigit;
+  return true;
+};
+
+const isNotDuplicatedNum = (value) => {
+  const valueSet = new Set(value);
+
+  return value.length === valueSet.size;
 };
 
 export const isValiUserInput = (value) => {
-  return !Number.isNaN(Number(value)) && isNumberInRange(value, NUMBER.MIN_DIGIT, NUMBER.MAX_DIGIT);
+  return (
+    !Number.isNaN(Number(value)) &&
+    isNumberInRange(value, NUMBER.MIN_DIGIT, NUMBER.MAX_DIGIT) &&
+    isNotDuplicatedNum(value)
+  );
 };
